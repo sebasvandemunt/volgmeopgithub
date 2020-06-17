5aca5b083be7cf0240d35a97e5d8614713643cdf
Sebas <56074252+sebasvandemunt@users.noreply.github.com>
2020-06-17 22:08:10 +0200

    Add files via upload

---
+++ Webapp Enigma Springs versie 2/javascript.js
@@ -0,0 +1,59 @@
+////* NEW CODE GEDAAN *////
+//KLIKBARE ELEMENTEN TEN ALLE TIJDEN
+
+//SHOE
+var clickableShoe = document.getElementById('shoe');
+
+function shoeChange() {
+  clickableShoe.classList.toggle('shoe2');
+
+}
+clickableShoe.addEventListener('click', shoeChange);
+
+
+//SALVATOGRE
+var clickableSalvatogre = document.getElementById('salvatogre');
+
+function salvatogreChange() {
+  clickableSalvatogre.classList.toggle('salvatogre2');
+
+}
+clickableSalvatogre.addEventListener('click', salvatogreChange);
+
+
+//FACEVASE
+var clickablefacevase = document.getElementById('facevase');
+
+function facevaseChange() {
+  clickablefacevase.classList.toggle('facevase2');
+  
+}
+clickablefacevase.addEventListener('click', facevaseChange)
+
+
+//MAYOR DEAD BODY
+var clickablemayor = document.getElementById('mayor');
+
+function mayorChange() {
+  clickablemayor.classList.toggle('mayor2');
+
+}
+clickablemayor.addEventListener('click', mayorChange);
+
+//GINGERBREAD
+var clickablegingerbread = document.getElementById('gingerbread');
+
+function cookiechange() {
+  clickablegingerbread.classList.toggle('gingerbread2');
+
+}
+clickablegingerbread.addEventListener('click', cookiechange);
+
+//KAT
+var clickablekat = document.getElementById('kat');
+
+function katchange() {
+  clickablekat.classList.toggle('kat2');
+
+}
+clickablekat.addEventListener('click', katchange);
